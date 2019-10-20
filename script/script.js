bd0eadd9d496fd5752d4d47ec8a6cf270f56f8de
Aleksey Batnin <al-bat-ros@mail.ru>
2019-10-21 03:01:55 +0500

    added values

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
+alert('Hello world!!!');
 console.log('Hello world');
+
+let money = 1000; 
+let income = 'Фриланс'; 
+let addExpenses = 'еда, бензин, спортзал'; 
+let deposit = true; 
+let mission = 100000000; 
+let period = 5;
