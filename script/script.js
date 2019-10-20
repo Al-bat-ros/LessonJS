0e1c5c94481a0b7190dd2b5625641521de6225e2
Aleksey Batnin <al-bat-ros@mail.ru>
2019-10-21 03:46:47 +0500

    exit
    clear
    enter

--- script/script.js
+++ script/script.js
@@ -1,9 +1,22 @@
-alert('Hello world!!!');
+//alert('Hello world!!!');
 console.log('Hello world');
 
 let money = 1000; 
 let income = 'Фриланс'; 
-let addExpenses = 'еда, бензин, спортзал'; 
+let addExpenses = 'Eда, Бензин, Спортзал'; 
 let deposit = true; 
 let mission = 100000000; 
 let period = 5;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(income.length);
+console.log('"'+'Период' + ' ' + period + ' ' + 'месяцев'+'"' );
+console.log(`"Цель заработать ${mission}$ долларов"`);
+console.log(addExpenses.toLowerCase().split());
+
+let budgetDay = money / 30;
+
+console.log(budgetDay)
+console.log((money % 30))
